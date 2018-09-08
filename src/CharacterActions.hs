94e8db98ada463253d2a22094eb7597163e779f5
Ashley Smith <ashley@aas.sh>
2018-09-08 23:08:59 +0100

    Basic randomisation of damage in - Characters basically deal 1-str damage

--- src/CharacterActions.hs
+++ src/CharacterActions.hs
@@ -7,6 +7,8 @@ module CharacterActions
 import Apecs
 import SDL.Vect
 
+import System.Random
+
 import Common
 import Components
 import Characters
@@ -17,21 +19,23 @@ attack a v = do
   ac :: Character <- get a
   vc :: Character <- get v
   Position pos <- get v
-  let damage = getDamage ac vc
-      vc' = dealDamage damage vc
+  damage <- liftIO $ getDamage ac vc
+  let vc' = dealDamage damage vc
   set v vc'
   spawnFloatingText (show damage) (V4 255 0 0 255) pos
   if health vc' > 0 then do
     postMessage $ name ac ++ " attacks " ++ name vc' ++ " for " ++ show damage ++ " damage!"
     postMessage $ name vc' ++ " has " ++ show (health vc') ++ " health left!"
   else
-    postMessage $ name ac ++ " kills " ++ name vc' ++ " with " ++ show (health vc') ++ " overkill damage!"
+    postMessage $ name ac ++ " kills " ++ name vc' ++ " with " ++ show (negate $ health vc') ++ " overkill damage!"
 
--- Get the damage to be dealt
-getDamage :: Character -> Character -> Int
-getDamage atk def = max (dam - def) 0
-  where dam = strength $ stats atk
-        def = 0
+-- Get the damage to be dealt using the IO monad
+getDamage :: Character -> Character -> IO Int
+getDamage atk def = do
+  r <- getStdRandom (randomR (1, strength $ stats atk))
+  let dam = r
+      def = 0
+  pure $ max (dam - def) 0
 
 -- Deal simple damage to enemy health
 dealDamage :: Int -> Character -> Character
